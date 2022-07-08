b2e8316cddbda0e652539b3e92ba6cfb2e6a446c
Bria Whitehead <whitehead.bria@protonmail.com>
2022-07-07 23:37:32 -0500

    nothing really added. just saved and wanted to make sure everything was correct

--- QuickStart.java
+++ QuickStart.java
@@ -1,5 +1,5 @@
 class QuickStart {
     public static void main (String[] args) {
-        System.out.print("Hello World. This is a test!");
+        System.out.print("Hello World. This is a");
     }
 }
